42b2355ac7d3abc05b72b756eba21160e2bda3c6
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-10-01 10:02:52 -0500

    deps: add jest 29.1.1

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
