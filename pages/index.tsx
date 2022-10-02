15997c2079e66c0cc667a90e51a145874f792447
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-10-02 17:45:08 -0500

    feat: add basic page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,16 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <section className="min-h-screen grid grid-cols-12 gap-1 grid-rows-[auto_auto_1fr] md:grid-rows-[auto_1fr]">
       <Head>
         <title>Blacksmith</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+      <header className="col-span-full"></header>
+      <aside className="col-span-full md:col-span-2"></aside>
+      <main className="col-span-full md:col-span-10"></main>
+      <footer className="col-span-full"></footer>
+    </section>
   );
 };
 
