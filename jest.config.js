0ebe179935fe6a86270cb4cd02a740a38fb33ce3
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-10-04 23:02:40 -0500

    chore: add jest dom matchers

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
 
 module.exports = createJestConfig(customJestConfig);
