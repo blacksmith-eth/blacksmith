6286e02c960dd45a53db7f6756aedae08c4c041e
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-10-05 20:29:37 -0500

    feat: add advanced layout

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,8 +1,75 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "testing";
 import Home from "pages/index";
 
 describe("Home", () => {
   it("should render without crashing", () => {
     render(<Home />);
+  });
+
+  it("should have a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      level: 1,
+      name: "blacksmith",
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should open and close the wallet", async () => {
+    const { user } = render(<Home />);
+
+    const openWalletButton = screen.getByRole("button", {
+      name: "open wallet",
+    });
+
+    expect(openWalletButton).toBeInTheDocument();
+
+    await user.click(openWalletButton);
+
+    expect(
+      screen.getByRole("heading", { level: 2, name: "wallet" })
+    ).toBeInTheDocument();
+
+    const closeWalletButton = screen.getByRole("button", {
+      name: "close wallet",
+    });
+
+    expect(closeWalletButton).toBeInTheDocument();
+
+    await user.click(closeWalletButton);
+
+    expect(
+      screen.queryByRole("heading", { level: 2, name: "wallet" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("should open and close the drawer", async () => {
+    const { user } = render(<Home />);
+
+    const openDrawerButton = screen.getByRole("button", {
+      name: "open drawer",
+    });
+
+    expect(openDrawerButton).toBeInTheDocument();
+
+    await user.click(openDrawerButton);
+
+    expect(
+      screen.getByRole("heading", { level: 2, name: "drawer" })
+    ).toBeInTheDocument();
+
+    const closeDrawerButton = screen.getByRole("button", {
+      name: "close drawer",
+    });
+
+    expect(closeDrawerButton).toBeInTheDocument();
+
+    await user.click(closeDrawerButton);
+
+    expect(
+      screen.queryByRole("heading", { level: 2, name: "drawer" })
+    ).not.toBeInTheDocument();
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,57 @@
+import { useToggle } from "hooks";
 import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+const Wallet = ({ open }: { open: boolean }) => {
+  if (!open) return <></>;
   return (
-    <section className="min-h-screen grid grid-cols-12 gap-1 grid-rows-[auto_auto_1fr] md:grid-rows-[auto_1fr]">
+    <aside className="bg-white border-l p-2 fixed right-0 h-full w-full lg:w-96 overflow-y-auto overscroll-none">
+      <h2>wallet</h2>
+    </aside>
+  );
+};
+
+const Drawer = ({ open }: { open: boolean }) => {
+  if (!open) return <></>;
+  return (
+    <section className="h-52 overflow-y-auto overscroll-none">
+      <h2>drawer</h2>
+    </section>
+  );
+};
+
+const Home: NextPage = () => {
+  const { state: isWalletOpen, toggle: toggleWallet } = useToggle(false);
+  const { state: isDrawerOpen, toggle: toggleDrawer } = useToggle(false);
+
+  const walletButtonText = isWalletOpen ? "close wallet" : "open wallet";
+  const drawerButtonText = isDrawerOpen ? "close drawer" : "open drawer";
+
+  return (
+    <section className="min-h-screen max-h-screen flex flex-col overflow-hidden">
       <Head>
         <title>Blacksmith</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="col-span-full"></header>
-      <aside className="col-span-full md:col-span-2"></aside>
-      <main className="col-span-full md:col-span-10"></main>
-      <footer className="col-span-full"></footer>
+      <header className="bg-white border-b sticky top-0 p-2 flex justify-between">
+        <h1>blacksmith</h1>
+        <button onClick={toggleWallet}>{walletButtonText}</button>
+      </header>
+      <main className="bg-white flex flex-col md:flex-row flex-grow overflow-y-auto overscroll-none">
+        <aside className="bg-white border-r p-2 w-full md:static md:basis-1/5 md:overflow-y-auto md:overscroll-none">
+          <h2>contracts</h2>
+        </aside>
+        <section className="flex flex-col flex-grow">
+          <section className="bg-white p-2 flex-grow overflow-y-auto overscroll-none">
+            <h2>contract</h2>
+          </section>
+          <section className="bg-white border-t sticky bottom-0 p-2">
+            <button onClick={toggleDrawer}>{drawerButtonText}</button>
+            <Drawer open={isDrawerOpen} />
+          </section>
+        </section>
+        <Wallet open={isWalletOpen} />
+      </main>
     </section>
   );
 };
