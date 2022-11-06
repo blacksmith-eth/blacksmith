9968fe85480967171bca7ed7305f4b7ce2affef1
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-11-06 12:08:33 -0600

    refactor: extract inline drawer prop type

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -1,4 +1,8 @@
-const Drawer = ({ open }: { open: boolean }) => {
+type DrawerProps = {
+  open: boolean;
+};
+
+const Drawer = ({ open }: DrawerProps) => {
   if (!open) return <></>;
   return (
     <section className="h-52 overflow-y-auto overscroll-none">
