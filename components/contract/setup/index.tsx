f6613b72f6d84ffe3423e052ccd0e96b0305fd49
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-11-05 20:38:33 -0500

    feat: add code block style to deploy script

--- components/contract/setup/index.tsx
+++ components/contract/setup/index.tsx
@@ -1,3 +1,4 @@
+import { Square2StackIcon } from "@heroicons/react/24/outline";
 import Field from "components/field";
 import { useState } from "react";
 
@@ -12,6 +13,7 @@ const Setup = () => {
   const [verifierUrl, setVerifierUrl] = useState(
     "http://localhost:3000/api/verify"
   );
+  const command = `forge create ${path}:${contractName} --verify --unlocked --from ${deployerAddress} --rpc-url ${rpcUrl} --verifier-url ${verifierUrl} --etherscan-api-key blacksmith`;
 
   const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDeployerAddress(event.target.value);
@@ -43,130 +45,149 @@ const Setup = () => {
 
   const focusedClasses = "font-bold underline";
 
+  const handleCopyCommand = () => {
+    navigator.clipboard.writeText(command);
+  };
+
   return (
     <div className="flex flex-col">
       <h3 className="font-bold text-2xl">Getting Started</h3>
-      <p>
-        To get started, you will need to deploy a contract to a local testnet
-        node like{" "}
-        <a
-          href="https://github.com/foundry-rs/foundry/tree/master/anvil"
-          className="underline"
+      <div className="flex flex-col gap-2">
+        <div>
+          <p>
+            To get started, you will need to deploy a contract to a local
+            testnet node like{" "}
+            <a
+              href="https://github.com/foundry-rs/foundry/tree/master/anvil"
+              className="underline"
+            >
+              Anvil
+            </a>
+            .
+          </p>
+          <p>
+            You can use the form below to generate the necessary{" "}
+            <a
+              href="https://github.com/foundry-rs/foundry/tree/master/forge"
+              className="underline"
+            >
+              Forge
+            </a>{" "}
+            command to deploy your contract to the local testnet and see it
+            appear in Blacksmith.
+          </p>
+          <p>
+            In most cases you will only need to change the{" "}
+            <span className="italic">path</span> and{" "}
+            <span className="italic">contract name</span> fields.
+          </p>
+        </div>
+        <form
+          onSubmit={preventDefault}
+          onBlur={() => setFocusedInput("")}
+          className="flex flex-col gap-2"
         >
-          Anvil
-        </a>
-        .
-      </p>
-      <p>
-        You can use the form below to generate the necessary{" "}
-        <a
-          href="https://github.com/foundry-rs/foundry/tree/master/forge"
-          className="underline"
-        >
-          Forge
-        </a>{" "}
-        command to deploy your contract to the local testnet and see it appear
-        in Blacksmith.
-      </p>
-      <p>
-        In most cases you will only need to change the{" "}
-        <span className="italic">path</span> and{" "}
-        <span className="italic">contract name</span> fields.
-      </p>
-      <form
-        onSubmit={preventDefault}
-        onBlur={() => setFocusedInput("")}
-        className="flex flex-col gap-2"
-      >
-        <Field
-          id="path"
-          inputName="path"
-          type="string"
-          value={path}
-          handleChange={handlePathChange}
-          onFocus={() => setFocusedInput("path")}
-        />
-        <Field
-          id="contractName"
-          inputName="contract name"
-          type="string"
-          value={contractName}
-          handleChange={handleContractNameChange}
-          onFocus={() => setFocusedInput("contractName")}
-        />
-        <Field
-          id="deployerAddress"
-          inputName="deployer address"
-          type="address"
-          value={deployerAddress}
-          handleChange={handleAddressChange}
-          onFocus={() => setFocusedInput("deployerAddress")}
-        />
-        <Field
-          id="rpcUrl"
-          inputName="rpc url"
-          type="string"
-          value={rpcUrl}
-          handleChange={handleRpcUrlChange}
-          onFocus={() => setFocusedInput("rpcUrl")}
-        />
-        <Field
-          id="verifierUrl"
-          inputName="verifier url"
-          type="string"
-          value={verifierUrl}
-          handleChange={handleVerifierUrlChange}
-          onFocus={() => setFocusedInput("verifierUrl")}
-        />
-      </form>
-      <div>
-        <div>
-          {`forge create `}
-          <span className={`${focusedInput === "path" ? focusedClasses : ""}`}>
-            {path}
-          </span>
-          :
-          <span
-            className={`${
-              focusedInput === "contractName" ? focusedClasses : ""
-            }`}
+          <Field
+            id="path"
+            inputName="path"
+            type="string"
+            value={path}
+            handleChange={handlePathChange}
+            onFocus={() => setFocusedInput("path")}
+          />
+          <Field
+            id="contractName"
+            inputName="contract name"
+            type="string"
+            value={contractName}
+            handleChange={handleContractNameChange}
+            onFocus={() => setFocusedInput("contractName")}
+          />
+          <Field
+            id="deployerAddress"
+            inputName="deployer address"
+            type="address"
+            value={deployerAddress}
+            handleChange={handleAddressChange}
+            onFocus={() => setFocusedInput("deployerAddress")}
+          />
+          <Field
+            id="rpcUrl"
+            inputName="rpc url"
+            type="string"
+            value={rpcUrl}
+            handleChange={handleRpcUrlChange}
+            onFocus={() => setFocusedInput("rpcUrl")}
+          />
+          <Field
+            id="verifierUrl"
+            inputName="verifier url"
+            type="string"
+            value={verifierUrl}
+            handleChange={handleVerifierUrlChange}
+            onFocus={() => setFocusedInput("verifierUrl")}
+          />
+        </form>
+        <div className="flex items-start justify-between font-mono text-sm p-4 bg-slate-100 rounded group">
+          <div>
+            <div>
+              {`forge create `}
+              <span
+                className={`${focusedInput === "path" ? focusedClasses : ""}`}
+              >
+                {path}
+              </span>
+              :
+              <span
+                className={`${
+                  focusedInput === "contractName" ? focusedClasses : ""
+                }`}
+              >
+                {contractName}
+              </span>
+              {` --verify --unlocked \\`}
+            </div>
+            <div>
+              {`--from `}
+              <span
+                className={`${
+                  focusedInput === "deployerAddress" ? focusedClasses : ""
+                }`}
+              >
+                {deployerAddress}
+              </span>
+              {` \\`}
+            </div>
+            <div>
+              {`--rpc-url `}
+              <span
+                className={`${focusedInput === "rpcUrl" ? focusedClasses : ""}`}
+              >
+                {rpcUrl}
+              </span>
+              {` \\`}
+            </div>
+            <div>
+              {`--verifier-url `}
+              <span
+                className={`${
+                  focusedInput === "verifierUrl" ? focusedClasses : ""
+                }`}
+              >
+                {verifierUrl}
+              </span>
+              {` \\`}
+            </div>
+            <div>{`--etherscan-api-key blacksmith`}</div>
+          </div>
+          <button
+            className="text-slate-600 hover:bg-slate-200 focus:outline-none focus:bg-slate-200 hover:text-slate-800 focus:text-slate-800 active:text-slate-900 active:bg-slate-300 border border-slate-400 rounded p-2"
+            onClick={handleCopyCommand}
           >
-            {contractName}
-          </span>
-          {` --verify --unlocked \\`}
+            <span className="sr-only">Copy</span>
+            <Square2StackIcon className="h-4 w-4" />
+          </button>
         </div>
-        <div>
-          {`--from `}
-          <span
-            className={`${
-              focusedInput === "deployerAddress" ? focusedClasses : ""
-            }`}
-          >
-            {deployerAddress}
-          </span>
-          {` \\`}
-        </div>
-        <div>
-          {`--rpc-url `}
-          <span
-            className={`${focusedInput === "rpcUrl" ? focusedClasses : ""}`}
-          >
-            {rpcUrl}
-          </span>
-          {` \\`}
-        </div>
-        <div>
-          {`--verifier-url `}
-          <span
-            className={`${
-              focusedInput === "verifierUrl" ? focusedClasses : ""
-            }`}
-          >
-            {verifierUrl}
-          </span>
-          {` \\`}
-        </div>
-        <div>{`--etherscan-api-key blacksmith`}</div>
       </div>
     </div>
   );
