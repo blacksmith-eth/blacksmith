fde07944fca21a303e4cb518737226b4312257c9
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-11-28 20:15:01 -0600

    feat: add etherscan contract import

--- components/contract/manager/index.tsx
+++ components/contract/manager/index.tsx
@@ -1,14 +1,31 @@
-import { ArchiveBoxXMarkIcon } from "@heroicons/react/24/outline";
+import {
+  ArchiveBoxXMarkIcon,
+  PlusSmallIcon,
+} from "@heroicons/react/24/outline";
 import Field from "components/field";
 import { useState } from "react";
 import { useSWRConfig } from "swr";
 
 const Manager = () => {
   const [address, setAddress] = useState("");
+  const [etherscanApiKey, setEtherscanApiKey] = useState("");
   const { mutate } = useSWRConfig();
 
   const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAddress(event.target.value);
+  };
+
+  const handleEtherscanApiKeyChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEtherscanApiKey(event.target.value);
+  };
+
+  const handleImport = () => {
+    fetch(`/api/contracts/${address}`, {
+      method: "POST",
+      body: JSON.stringify({ key: etherscanApiKey }),
+    }).then(() => mutate("/api/contracts"));
   };
 
   const handleRemove = () => {
@@ -24,30 +41,69 @@ const Manager = () => {
   };
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col">
       <h3 className="font-bold text-2xl">Contract Management</h3>
-      <Field
-        inputName="contract address"
-        value={address}
-        type="address"
-        id="address"
-        handleChange={handleAddressChange}
-      />
-      <button
-        onClick={handleRemove}
-        className="self-start flex items-center gap-1 border border-black px-1.5 py-0.5 rounded"
-      >
-        <span className="text-sm">remove</span>
-        <ArchiveBoxXMarkIcon className="h-4 w-4" />
-      </button>
-      <h4 className="font-bold">Danger Zone</h4>
-      <button
-        onClick={handleRemoveAll}
-        className="self-start flex items-center gap-1 border border-black px-1.5 py-0.5 rounded"
-      >
-        <span className="text-sm">remove all</span>
-        <ArchiveBoxXMarkIcon className="h-4 w-4" />
-      </button>
+      <div className="flex flex-col gap-2">
+        <div>
+          <p>
+            You can remove any contract from Blacksmith that you have imported
+            or verified without an Etherscan API key.
+          </p>
+          <p>
+            You can import verified contracts from Etherscan by providing a
+            contract address and an Etherscan API key.
+          </p>
+          <p>
+            You can interact with imported contracts by starting an instance of
+            Anvil that{" "}
+            <a
+              href="https://book.getfoundry.sh/tutorials/forking-mainnet-with-cast-anvil"
+              className="underline"
+            >
+              forks mainnet
+            </a>
+            .
+          </p>
+        </div>
+        <Field
+          inputName="contract address"
+          value={address}
+          type="address"
+          id="address"
+          handleChange={handleAddressChange}
+        />
+        <Field
+          inputName="etherscan api key"
+          value={etherscanApiKey}
+          type="string"
+          id="etherscan-api-key"
+          handleChange={handleEtherscanApiKeyChange}
+        />
+        <div className="flex flex-row gap-2">
+          <button
+            onClick={handleImport}
+            className="self-start flex items-center gap-1 border border-black px-1.5 py-0.5 rounded"
+          >
+            <span className="text-sm">import</span>
+            <PlusSmallIcon className="w-4 h-4" />
+          </button>
+          <button
+            onClick={handleRemove}
+            className="self-start flex items-center gap-1 border border-black px-1.5 py-0.5 rounded"
+          >
+            <span className="text-sm">remove</span>
+            <ArchiveBoxXMarkIcon className="h-4 w-4" />
+          </button>
+        </div>
+        <h4 className="font-bold">Danger Zone</h4>
+        <button
+          onClick={handleRemoveAll}
+          className="self-start flex items-center gap-1 border border-black px-1.5 py-0.5 rounded"
+        >
+          <span className="text-sm">remove all</span>
+          <ArchiveBoxXMarkIcon className="h-4 w-4" />
+        </button>
+      </div>
     </div>
   );
 };
--- pages/api/contracts/[address].ts
+++ pages/api/contracts/[address].ts
@@ -6,6 +6,31 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method === "POST") {
+    const ETHERSCAN_API_KEY = req.body.key;
+    return fetch(
+      `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${req.query.address}&apikey=${ETHERSCAN_API_KEY}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const { ABI, ContractName, CompilerVersion } = data.result[0];
+        contract.insert({
+          abi: JSON.parse(ABI),
+          address: req.query.address as Address,
+          name: ContractName,
+          version: CompilerVersion,
+        });
+        return res.status(200).json({});
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
   if (req.method === "DELETE") {
     await contract.remove(req.query.address as Address);
     return res.status(200).json({});
