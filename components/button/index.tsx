1b8ac2b832e8dd86133a85413f7297006c7a0fbe
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-11-29 19:11:41 -0600

    feat: add button state styles

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -6,7 +6,7 @@ type ButtonProps = PropsWithChildren<{}> &
 const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
-      className="self-start border border-black dark:border-white px-2 py-0.5 text-sm disabled:cursor-not-allowed disabled:opacity-50 hover:bg-slate-200 dark:hover:bg-black"
+      className="self-start border border-black dark:border-white px-2 py-0.5 text-sm disabled:cursor-not-allowed disabled:opacity-50 hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-white dark:hover:text-black dark:focus:bg-white dark:focus:text-black focus:outline-none"
       {...props}
     >
       {children}
