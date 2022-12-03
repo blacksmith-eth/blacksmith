64dc5ef961a1873c9c803cf8ac04a8f51e283071
Sam Barnes <mistersamuelbarnes@gmail.com>
2022-12-03 16:40:37 -0700

    feat: collapsible function signatures

--- components/function/view/index.tsx
+++ components/function/view/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Button from "components/button";
 import Inputs from "components/inputs";
 import {
@@ -27,21 +28,25 @@ const View = ({ address, func }: ViewProps) => {
     args: formattedArgs,
     watch: true,
   });
+  const [collapsed, setCollapsed] = React.useState(true)
 
   return (
     <li key={func.name} className="flex flex-col gap-2">
-      <Signature func={func} />
-      <Inputs name={func.name} args={args} updateValue={updateValue} />
-      <Container>
-        <Button onClick={() => refetch()}>read</Button>
-        <Output
-          data={data}
-          isTouched={isTouched}
-          isLoading={isLoading}
-          isError={isError}
-          error={error}
-        />
-      </Container>
+      <Signature func={func} collapsed={collapsed} setCollapsed={setCollapsed} />
+
+      { !collapsed && (<>
+        <Inputs name={func.name} args={args} updateValue={updateValue} />
+        <Container>
+          <Button onClick={() => refetch()}>read</Button>
+          <Output
+            data={data}
+            isTouched={isTouched}
+            isLoading={isLoading}
+            isError={isError}
+            error={error}
+          />
+        </Container>
+      </>)}
     </li>
   );
 };
