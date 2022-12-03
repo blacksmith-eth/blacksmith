17872761146981f853efa0460ea7c1aa1439825a
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-03 15:06:50 -0600

    feat: add contract management responses

---
+++ components/contract/manager/index.test.tsx
@@ -0,0 +1,59 @@
+import { server } from "mocks/server";
+import { rest } from "msw";
+import { render, screen } from "testing";
+import { buildAddress } from "testing/factory";
+import Manager from ".";
+
+describe("Manager", () => {
+  it("should handle contract imports", async () => {
+    const address = buildAddress();
+    server.use(
+      rest.post(`/api/contracts/${address}`, (_req, res, ctx) => {
+        return res(ctx.json({}));
+      })
+    );
+    const { user } = render(<Manager />);
+    const input = screen.getByLabelText("contract address");
+
+    await user.type(input, address);
+
+    const button = screen.getByRole("button", { name: "import" });
+
+    await user.click(button);
+
+    await screen.findByText("Imported successfully");
+  });
+
+  it("should handle contract removals", async () => {
+    const address = buildAddress();
+    server.use(
+      rest.delete(`/api/contracts/${address}`, (_req, res, ctx) => {
+        return res(ctx.json({}));
+      })
+    );
+    const { user } = render(<Manager />);
+    const input = screen.getByLabelText("contract address");
+
+    await user.type(input, address);
+
+    const button = screen.getByRole("button", { name: "remove" });
+
+    await user.click(button);
+
+    await screen.findByText("Removed successfully");
+  });
+
+  it("should handle all contract removals", async () => {
+    server.use(
+      rest.delete(`/api/contracts`, (_req, res, ctx) => {
+        return res(ctx.json({}));
+      })
+    );
+    const { user } = render(<Manager />);
+    const button = screen.getByRole("button", { name: "remove all" });
+
+    await user.click(button);
+
+    await screen.findByText("Removed all contracts");
+  });
+});
