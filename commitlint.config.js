9b02e442d4144bda0945512c3d5a9f8f384f2f24
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-05 17:14:50 -0600

    chore: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "deps",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
