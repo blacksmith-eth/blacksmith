7088d1e01ce6154f6b0a9a3d31d89b6f5d085b60
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-12 00:24:57 -0600

    refactor: extract unit component

--- components/function/payable/index.tsx
+++ components/function/payable/index.tsx
@@ -1,7 +1,7 @@
-import { Listbox } from "@headlessui/react";
 import Button from "components/button";
 import Field from "components/field";
 import Inputs from "components/inputs";
+import Unit from "components/unit";
 import {
   AbiDefinedFunction,
   AbiParameterWithComponents,
@@ -55,31 +55,7 @@ const Payable = ({ address, func }: PayableProps) => {
             type="uint256"
             handleChange={handleValueChange}
           />
-          <Listbox value={unit} onChange={setUnit}>
-            <Listbox.Label className="sr-only">unit</Listbox.Label>
-            <div className="relative text-right select-none">
-              <Listbox.Button className="border border-black dark:border-white h-full w-full px-2 text-sm hover:bg-black hover:text-white focus:bg-black focus:text-white dark:hover:bg-white dark:hover:text-black dark:focus:bg-white dark:focus:text-black focus:outline-none">
-                {unit}
-              </Listbox.Button>
-              <Listbox.Options className="bg-white dark:bg-black border border-black dark:border-white absolute mt-1 right-0 focus:outline-none z-10">
-                {units.map((unit) => (
-                  <Listbox.Option
-                    key={unit}
-                    value={unit}
-                    className={({ active }) =>
-                      `${
-                        active
-                          ? "bg-black dark:bg-white text-white dark:text-black"
-                          : ""
-                      } px-2`
-                    }
-                  >
-                    {unit}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </div>
-          </Listbox>
+          <Unit units={units} unit={unit} setUnit={setUnit} />
         </div>
       </section>
       <Inputs name={func.name} args={args} updateValue={updateValue} />
