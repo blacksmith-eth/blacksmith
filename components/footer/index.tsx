24e3828d93d0517c7ce6f4d2ddceae0fdd8f8b21
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-16 20:51:01 -0600

    refactor: extract footer component

---
+++ components/footer/index.tsx
@@ -0,0 +1,24 @@
+import Anchor from "components/anchor";
+import Switch from "components/switch";
+
+const Footer = () => (
+  <footer className="flex flex-col">
+    <Switch />
+    <div className="text-sm px-1.5">
+      <span>
+        Created by{" "}
+        <Anchor href="https://twitter.com/0xholypanda">0xholypanda</Anchor>
+      </span>
+      <span> | </span>
+      <Anchor href="https://github.com/blacksmith-eth/blacksmith">
+        star on github
+      </Anchor>
+      <span> | </span>
+      <Anchor href="https://github.com/blacksmith-eth/blacksmith/issues/new">
+        report an issue
+      </Anchor>
+    </div>
+  </footer>
+);
+
+export default Footer;
