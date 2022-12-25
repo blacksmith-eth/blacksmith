4dd7b4298ab0e041ec4bf99758f17695985baa51
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-25 18:20:04 -0500

    refactor: button use named export

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 type ButtonProps = PropsWithChildren<{}> &
   ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, ...props }: ButtonProps) => {
+export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
       className="self-start border border-black dark:border-white px-2 py-0.5 text-sm disabled:cursor-not-allowed disabled:opacity-50 hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-white dark:hover:text-black dark:focus:bg-white dark:focus:text-black focus:outline-none"
@@ -13,5 +13,3 @@ const Button = ({ children, ...props }: ButtonProps) => {
     </button>
   );
 };
-
-export default Button;
