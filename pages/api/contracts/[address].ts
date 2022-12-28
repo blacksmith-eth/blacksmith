42b4f8884733eefcb19813031e92eab9f94169c3
holypanda <114544906+unholypanda@users.noreply.github.com>
2022-12-28 15:35:52 -0500

    refactor: parse request params using zod

--- pages/api/contracts/[address].ts
+++ pages/api/contracts/[address].ts
@@ -1,7 +1,9 @@
 import { contract } from "core/contract";
 import type { Address } from "core/types";
 import { getAddress } from "core/utils";
+import isNull from "lodash/isNull";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
 
 const BASE_URL = "https://api.etherscan.io";
 
@@ -12,12 +14,27 @@ const buildGetSourceCodeUrl = (address: Address) => {
   }`;
 };
 
+const requestSchema = z.object({
+  address: z.custom<Address>().transform((value, context) => {
+    const address = getAddress(value);
+    if (isNull(address)) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid address",
+      });
+
+      return z.NEVER;
+    }
+    return address;
+  }),
+});
+
 const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const address = getAddress(req.query.address as Address);
-  if (!address) {
-    return res.status(400).json({ error: "Invalid address" });
+  const result = requestSchema.safeParse(req.query);
+  if (!result.success) {
+    return res.status(400).json({ error: result.error.message });
   }
-  return fetch(buildGetSourceCodeUrl(address), {
+  return fetch(buildGetSourceCodeUrl(result.data.address), {
     headers: {
       "Content-Type": "application/json",
     },
@@ -27,7 +44,7 @@ const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
       const { ABI, ContractName, CompilerVersion } = data.result[0];
       contract.insert({
         abi: JSON.parse(ABI),
-        address,
+        address: result.data.address,
         name: ContractName,
         version: CompilerVersion,
       });
@@ -37,11 +54,11 @@ const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 const deleteHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const address = getAddress(req.query.address as Address);
-  if (!address) {
-    return res.status(400).json({ error: "Invalid address" });
+  const result = requestSchema.safeParse(req.query);
+  if (!result.success) {
+    return res.status(400).json({ error: result.error.message });
   }
-  await contract.remove(address);
+  await contract.remove(result.data.address);
   return res.status(200).json({});
 };
 
