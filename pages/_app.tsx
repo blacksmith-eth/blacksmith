149e47a3048e98491a1ab5057173f9e7cc8f250f
holypanda <114544906+unholypanda@users.noreply.github.com>
2023-01-21 18:25:52 -0600

    feat: add custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "styles/globals.css";
+import { Inter, Source_Code_Pro } from "@next/font/google";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import {
@@ -12,6 +13,16 @@ import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { blacksmithWallet } from "packages/wallets";
 import { forkedChains, foundry } from "core/chains";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-code-pro",
+});
 
 const { chains, provider, webSocketProvider } = configureChains(
   [foundry, ...forkedChains],
@@ -34,31 +45,33 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange={true}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          appInfo={{
-            appName: "Blacksmith",
-            learnMoreUrl: "https://github.com/blacksmith-eth/blacksmith",
-          }}
-          chains={chains}
-          theme={{
-            darkMode: darkTheme({
-              accentColor: "#ebddd1",
-              accentColorForeground: "#262a33",
-              borderRadius: "small",
-            }),
-            lightMode: lightTheme({
-              accentColor: "#262a33",
-              accentColorForeground: "#ebddd1",
-              borderRadius: "small",
-            }),
-          }}
-        >
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </ThemeProvider>
+    <div className={`${inter.variable} ${sourceCodePro.variable} font-sans`}>
+      <ThemeProvider attribute="class" disableTransitionOnChange={true}>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider
+            appInfo={{
+              appName: "Blacksmith",
+              learnMoreUrl: "https://github.com/blacksmith-eth/blacksmith",
+            }}
+            chains={chains}
+            theme={{
+              darkMode: darkTheme({
+                accentColor: "#ebddd1",
+                accentColorForeground: "#262a33",
+                borderRadius: "small",
+              }),
+              lightMode: lightTheme({
+                accentColor: "#262a33",
+                accentColorForeground: "#ebddd1",
+                borderRadius: "small",
+              }),
+            }}
+          >
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </ThemeProvider>
+    </div>
   );
 }
 
