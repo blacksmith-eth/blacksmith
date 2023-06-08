6ea79190e297f614575578d1b6f3452301d3d321
holypanda <114544906+unholypanda@users.noreply.github.com>
2023-06-07 23:32:09 -0400

    deps: upgrade vite packages to latest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
     globals: true,
